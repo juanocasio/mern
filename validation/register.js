171d2a1585536bbfad80d5837a534e76db82ee33
Ocasio, Juan A <jaocasio@brandsafway.com>
2018-07-17 16:41:59 -0400

    new validations

--- validation/register.js
+++ validation/register.js
@@ -9,9 +9,22 @@ module.exports = function validateRegisterInput(data){
     errors.name = 'Name must be between 2 and 30 characters';
   }
 
+  if (!Validator.isEmail(data.email)){
+    errors.email = 'Please enter a valid email address';
+  }
+
+  if(isEmpty(data.email)){
+    errors.email = 'Email field is required';
+  }
+  
+  if(!Validator.isLength(data.password, {min: 6, max:30})){
+    errors.password = 'Password length should be between 6 and 30 characters';
+  }
   if(isEmpty(data.password)){
     errors.password = 'Password required';
   }
+
+
 
   return {
     errors,
